261bf1b197cd55ffb689e3c94b4338f24e03d4b1
dassiorleando <me@dassiorleando.xyz>
2016-08-23 09:26:58 +0100

    Define a Procfile for starting app in heroku

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ var app = require('express')();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
 
+app.set('port', (process.env.PORT || 5000));
+
 app.get('/', function(req, res){
   res.sendFile(__dirname + '/index.html');
 });
@@ -13,6 +15,6 @@ io.on('connection', function(socket){
   });
 });
 
-http.listen(3000, function(){
-  console.log('Dassi Orleando simple chat app listening on *:3000');
+http.listen(app.get('port'), function(){
+  console.log('Dassi Orleando simple chat app listening on *:' + app.get('port'));
 });
