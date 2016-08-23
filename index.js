36a180fe8bb6ad38d9ac2fcded7fd8980094f741
dassiorleando <me@dassiorleando.xyz>
2016-08-23 10:59:06 +0100

    improve lauchn script to make socket.io work with heroku and change cdn to use https

--- index.js
+++ index.js
@@ -1,20 +1,20 @@
 var app = require('express')();
 var http = require('http').Server(app);
-var io = require('socket.io')(http);
+var io = require('socket.io').listen(http);
 
-app.set('port', (process.env.PORT || 5000));
+app.set('port', (process.env.PORT));
 
 app.get('/', function(req, res){
   res.sendFile(__dirname + '/index.html');
 });
 
-io.on('connection', function(socket){
+io.sockets.on('connection', function(socket){
   socket.on('chat message', function(msg){
     console.log(msg);
     io.emit('chat message', msg);
   });
 });
 
-http.listen(app.get('port'), function(){
+http.listen(process.env.PORT, function(){
   console.log('Dassi Orleando simple chat app listening on *:' + app.get('port'));
 });
